802a248e3a8514f2883cb716d921703c346c9823
Jonathan Tito <jotitoon@uide.edu.ec>
2026-05-19 01:27:00 +0000

    wip: first commands and C code

---
+++ s2.sh
@@ -0,0 +1,18 @@
+# 
+man gcc
+# 
+gcc --help
+# 
+gcc hola.c -o hola_binario
+#
+file hola_binario
+# 
+file hola.c
+#
+pwd
+#
+cd
+# ruta absoluta: es la ruta completa desde el directorio \
+# ruta relativa: no tiene la ruta completa
+# lista archivos y directorios en el directorio actual
+ls
